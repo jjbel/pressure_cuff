36cbe848a931dc7e92a39038b7c38f8155a235cd
jjbel <jaibellare@gmail.com>
2025-01-01 00:38:43 +0530

    sensor: get linear fit using mercury bp app

--- src/sketch_03_sensor_test/src/main.cpp
+++ src/sketch_03_sensor_test/src/main.cpp
@@ -1,4 +1,5 @@
 #include "HX711.h"
+#include <AFMotor.h>
 #include <Arduino.h>
 
 #include "sensor_init.h"
@@ -9,12 +10,16 @@ HX711 sensor;
 int frame = 0;
 uint64_t time = 0;
 
+AF_DCMotor motor = AF_DCMotor(1);
+
 void setup() {
   Serial.begin(38400);
-  sensor_init(sensor, A1, A0, 2280.f);
+  sensor_init(sensor, A1, A0, 1.f);
 
   frame = 0;
   time = millis();
+
+  delay(3000);
 }
 
 float read() {
@@ -30,6 +35,15 @@ float read() {
 }
 
 void loop() {
-  println(frame, millis(), read());
+  float factor = 255 * 0.25;
+  motor.setSpeed(factor);
+  motor.run(FORWARD);
+
+  const float val = float(sensor.read());
+
+  //   println(frame, /* millis(), */  +
+  //  1316000 /* sensor.get_offset(), read() */);
+//   println(val, val * 0.0000138368 + 18.38962, val * 0.0000138368 - 18.02885);
+  println(val * 0.0000138368 + 18.38962, "mm Hg");
   frame++;
 }

229cefc8010904626e815ff929eb1314a73b6a31
jjbel <jaibellare@gmail.com>
2025-01-06 13:07:31 +0530

    display: add Plotter

--- src/display_test.cpp
+++ src/display_test.cpp
@@ -9,8 +9,38 @@
 // https://youtu.be/4DtuOeeYHys
 // our display also needs reset pin
 
+struct Plotter {
+  Adafruit_ILI9341 &tft;
+
+  int16_t box_x;
+  int16_t box_y; // +ve y upward
+  int16_t box_w;
+  int16_t box_h;
+
+  float in_y_max;
+  int16_t out_x = 0;
+
+  void plot(float y) {
+    tft.drawPixel(box_x + out_x,
+                  box_y + box_h - static_cast<int16_t>(y / in_y_max * box_h),
+                  ILI9341_WHITE);
+
+    refresh();
+  }
+
+  void refresh() {
+    out_x++;
+    if (out_x >= box_w) {
+      out_x = 0;
+      tft.fillRect(box_x, box_y, box_w, box_h, ILI9341_BLACK);
+      Serial.println("refresh");
+    }
+  }
+};
+
 // Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
+Plotter plt = Plotter{tft, 5, 150, 300, 80, 80};
 
 void setup() {
   Serial.begin(38400);
@@ -62,6 +92,9 @@ void loop(void) {
   tft.print(i + 3);
   tft.setCursor(200, 64);
   tft.print(i + 4);
-  delay(500);
+
+  plt.plot(40 + 40 * sin(i / 50.0));
+
+  //   delay(100);
   i++;
 }

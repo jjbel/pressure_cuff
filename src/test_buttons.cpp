061716210add571a85c7393785d6a94d47135b7c
Jai Bellare <jaibellare@gmail.com>
2025-03-22 19:53:45 +0530

    test_buttons: test all 3

--- src/test_buttons.cpp
+++ src/test_buttons.cpp
@@ -1,18 +1,25 @@
 #include "Button.h"
 #include "Param.h"
+#include "util.h"
 
-Button button(2);
+Button button1(2);
+Button button2(3);
+Button button3(4);
 Param param_pressure{"pressure", 0, 10, 10, 10, 120};
 
 void setup() { Serial.begin(38400); }
 
 void loop() {
-  if (button.is_pressed()) {
+  if (button1.is_pressed()) {
     param_pressure.increment();
-    Serial.println(param_pressure.value);
+    // Serial.println(param_pressure.value);
   }
 
-  button.update();
+  button1.update();
+  button2.update();
+  button3.update();
+
+  println(button1.is_pressed(), button2.is_pressed(), button3.is_pressed());
 
   delay(100);
 }
